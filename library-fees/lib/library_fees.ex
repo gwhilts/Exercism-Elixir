2f002e8c653520f639a295d547b999675eece45f
Gerald Hilts <gwhilts@booknowl.com>
2022-11-11 22:56:40 -0800

    Implement 'Library Fees' exercise
    - All tests passing
    - Submitted/accepted

--- library-fees/lib/library_fees.ex
+++ library-fees/lib/library_fees.ex
@@ -1,25 +1,38 @@
 defmodule LibraryFees do
   def datetime_from_string(string) do
-    # Please implement the datetime_from_string/1 function
+    {:ok, datetime} = NaiveDateTime.from_iso8601(string)
+    datetime
   end
 
   def before_noon?(datetime) do
-    # Please implement the before_noon?/1 function
+    :lt == Time.compare(datetime, ~T[12:00:00])
   end
 
   def return_date(checkout_datetime) do
-    # Please implement the return_date/1 function
+    if before_noon?(checkout_datetime) do
+      Date.add(checkout_datetime, 28)
+    else
+      Date.add(checkout_datetime, 29)
+    end
   end
 
   def days_late(planned_return_date, actual_return_datetime) do
-    # Please implement the days_late/2 function
+    diff = Date.diff(actual_return_datetime, planned_return_date)
+    if diff >= 0, do: diff, else: 0
   end
 
   def monday?(datetime) do
-    # Please implement the monday?/1 function
+    1 == Date.day_of_week(datetime)
   end
 
   def calculate_late_fee(checkout, return, rate) do
-    # Please implement the calculate_late_fee/3 function
+    due = datetime_from_string(checkout) |> return_date()
+    actual = datetime_from_string(return)
+
+    days_late(due, actual) * rate * monday_discount(actual) |> trunc()
+  end
+
+  defp monday_discount(date) do
+    if monday?(date), do: 0.5, else: 1
   end
 end
