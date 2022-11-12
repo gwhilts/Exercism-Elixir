b5fe6cda5933bd4f055f60bc8d7da4e1f5dd4894
Gerald Hilts <gwhilts@booknowl.com>
2022-11-12 13:10:10 -0800

    Implement 'Basketball Website' exercise - accepted

--- basketball-website/lib/basketball_website.ex
+++ basketball-website/lib/basketball_website.ex
@@ -1,9 +1,13 @@
 defmodule BasketballWebsite do
-  def extract_from_path(data, path) do
-    # Please implement the extract_from_path/2 function
-  end
+  # Don't use Map or Kernel functions
+  @spec extract_from_path(map(), String.t()) :: any
+  def extract_from_path(data, path), do: extract_from_path(data, String.split(path, "."), "")
+  defp extract_from_path(_data, [], val), do: val
+  defp extract_from_path(data, [h | tail], _val), do: extract_from_path(data[h], tail, data[h])
 
+  # Use Kernel.get_in/2
+  @spec get_in_path(map(), String.t()) :: any
   def get_in_path(data, path) do
-    # Please implement the get_in_path/2 function
+    get_in(data, String.split(path, "."))
   end
 end
