fc71b7ca47644194f65c39dc5c7ff6174d07af79
Gerald Hilts <gwhilts@booknowl.com>
2022-11-13 12:12:08 -0800

    Implement 'Space Age' exercise - accepted

--- space-age/lib/space_age.ex
+++ space-age/lib/space_age.ex
@@ -15,5 +15,25 @@ defmodule SpaceAge do
   """
   @spec age_on(planet, pos_integer) :: {:ok, float} | {:error, String.t()}
   def age_on(planet, seconds) do
+    years = case planet do
+      :earth -> earth_years(seconds)
+      :mercury -> earth_years(seconds) / 0.2408467
+      :venus -> earth_years(seconds) / 0.61519726
+      :mars -> earth_years(seconds) / 1.8808158
+      :saturn -> earth_years(seconds) / 29.447498
+      :jupiter -> earth_years(seconds) / 11.862615
+      :uranus -> earth_years(seconds) / 84.016846
+      :neptune -> earth_years(seconds) / 164.79132
+      _ -> nil
+    end
+
+    case years do
+      nil -> {:error, "not a planet"}
+      _ -> {:ok, years}
+    end
+  end
+
+  defp earth_years(seconds) do
+    seconds / 31557600
   end
 end
