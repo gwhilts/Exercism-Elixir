7f9973539ba477951f9f680f1aa71566119072cd
Gerald Hilts <gwhilts@booknowl.com>
2022-11-14 14:53:39 -0800

    Implement 'Boutique Suggestions' exercise -unsub
    All test passing, but going to refactor
    a bit before submitting.

--- boutique-suggestions/lib/boutique_suggestions.ex
+++ boutique-suggestions/lib/boutique_suggestions.ex
@@ -1,5 +1,18 @@
 defmodule BoutiqueSuggestions do
-  def get_combinations(tops, bottoms, options) do
-    # Please implement the get_combinations/3 function
+  def get_combinations(tops, bottoms, options \\ []) do
+    for top <- tops, bottom <- bottoms, filter(top, bottom, options), do: {top, bottom}
+  end
+
+  defp filter(top, bottom, options) do
+    no_clash?(top, bottom)
+    && within_max_price?(top, bottom, options[:maximum_price])
+  end
+
+  defp no_clash?(top, bottom), do: top[:base_color] != bottom[:base_color]
+  defp within_max_price?(top, bottom, max_price) do
+    case max_price do
+      nil -> top[:price] + bottom[:price] <= 100
+      _ -> top[:price] + bottom[:price] <= max_price
+    end
   end
 end
