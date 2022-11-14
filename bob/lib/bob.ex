377a46853c555fa5ed2bce5c35adbc714491374b
Gerald Hilts <gwhilts@booknowl.com>
2022-11-14 13:19:22 -0800

    Implement 'Bob' exercise -accepted

--- bob/lib/bob.ex
+++ bob/lib/bob.ex
@@ -1,5 +1,18 @@
 defmodule Bob do
   @spec hey(String.t()) :: String.t()
   def hey(input) do
+    cond do
+      silence?(input) -> "Fine. Be that way!"
+      shouting_question?(input) -> "Calm down, I know what I'm doing!"
+      shouting?(input) -> "Whoa, chill out!"
+      question?(input) -> "Sure."
+      true -> "Whatever."
+    end
   end
+
+  defp question?(input), do: String.trim(input) |> String.ends_with?("?")
+  defp shouting?(input), do: (String.upcase(input) == input) && input =~ ~r/[A-ZА-Я]/
+  defp shouting_question?(input), do: shouting?(input) && question?(input)
+  defp silence?(input), do: "" == String.trim(input)
+
 end
