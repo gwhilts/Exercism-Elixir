365003f98f3b21b635b48d3fd025f3df24a8384f
Gerald Hilts <gwhilts@booknowl.com>
2022-11-16 16:17:14 -0800

    Implement 'RPN Calculator' exercise (submitted)
    All test passing. Submitted to Exercism but the CI
    never finished spinning. Posted anyway.
    
    Need to remember try/rescue instead of try/catch.

--- rpn-calculator/lib/rpn_calculator.ex
+++ rpn-calculator/lib/rpn_calculator.ex
@@ -1,13 +1,22 @@
 defmodule RPNCalculator do
+
   def calculate!(stack, operation) do
-    # Please implement the calculate!/2 function
+    operation.(stack)
   end
 
   def calculate(stack, operation) do
-    # Please implement the calculate/2 function
+    try do
+      {:ok, calculate!(stack, operation)}
+    rescue
+      _error -> :error
+    end
   end
 
   def calculate_verbose(stack, operation) do
-    # Please implement the calculate_verbose/2 function
+    try do
+      {:ok, calculate!(stack, operation)}
+    rescue
+      error -> {:error, error.message}
+    end
   end
 end
