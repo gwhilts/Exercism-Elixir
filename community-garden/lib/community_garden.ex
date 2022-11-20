44a8d85c21d3586b6bac907755c78f55e26867a3
Gerald Hilts <gwhilts@booknowl.com>
2022-11-20 12:48:44 -0800

    Implement 'Community Garden' exercise (accepted)
    Not sure if I structured the Agent data as well as
    I could have. Needed to maintain a set of stucts
    (%Plot{id: <int>, registered_to: <string>}), and the
    next plot ID to use.
    
    Originally, the data structure I used was
    
    %{next_id: n, plots: [%Plot{}, ...]}.
    
    The problem was that I needed to be able to find/update plots
    based on the id. There didn't seem to be a clean way
    to select a plot :plot_id without polling all the records.
    So I modifed the data structure to this instead:
    
    %{next_id: n, plots: %{n => %Plot{}, ...}}.
    
    It meant replicating the plot ID val, once within the
    struct, then again as a key to the struct in the map.

--- community-garden/lib/community_garden.ex
+++ community-garden/lib/community_garden.ex
@@ -6,26 +6,24 @@ end
 
 defmodule CommunityGarden do
   def start(_opts \\ []) do
-    Agent.start(fn() -> %{next_id: 1, plots: [] } end)
+    Agent.start(fn() -> %{next_id: 1, plots: %{} } end)
   end
   def list_registrations(pid) do
-    Agent.get pid, &Map.get(&1, :plots)
+    Agent.get pid, &Map.get(&1, :plots) |> Map.values()
   end
 
   def register(pid, register_to) do
     id = Agent.get pid, &Map.get(&1, :next_id)
     plot = %Plot{plot_id: id, registered_to: register_to}
-    Agent.update(pid, &( %{&1 | plots: [plot | &1[:plots]], next_id: id + 1}))
+    Agent.update(pid, &( %{&1 | plots: Map.put(&1[:plots], id, plot), next_id: id + 1}))
     plot
   end
 
   def release(pid, plot_id) do
-    Agent.get_and_update(pid,
-      fn(data) -> Map.pop(data[:plots], plot_id) end
-    )
+    Agent.update(pid, &%{&1 | plots: Map.delete(&1.plots, plot_id)})
   end
 
   def get_registration(pid, plot_id) do
-    # Please implement the get_registration/2 function
+    Agent.get(pid, &Map.get(&1[:plots], plot_id)) || {:not_found, "plot is unregistered"}
   end
 end
