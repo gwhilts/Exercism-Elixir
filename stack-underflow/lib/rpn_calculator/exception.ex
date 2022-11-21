90c97718f9e03e293bc09f5fe07ef4a239c680e1
Gerald Hilts <gwhilts@booknowl.com>
2022-11-21 10:01:16 -0800

    Implement 'Stack Underlow' exercise (accepted)
    Note:
    
    Not really sure what the @impl tag does. Just copied from the example
    page. Docs say it marks a functions as "implemented callbacks of a
    behaviour", but I'm not really groking that yet. I think it does
    something like generate a warning at compile time if the function is
    not a proper callback, but ???
    
    See https://hexdocs.pm/elixir/Module.html#module-impl
    
    Commenting it out does't seem to have effect; the tests still pass.

--- stack-underflow/lib/rpn_calculator/exception.ex
+++ stack-underflow/lib/rpn_calculator/exception.ex
@@ -1,5 +1,23 @@
 defmodule RPNCalculator.Exception do
-  # Please implement DivisionByZeroError here.
+  defmodule DivisionByZeroError do
+    defexception message: "division by zero occurred"
+  end
 
-  # Please implement StackUnderflowError here.
+  defmodule StackUnderflowError do
+    defexception message: "stack underflow occurred"
+
+    @impl true
+    def exception(context) do
+      case context do
+        [] -> %StackUnderflowError{}
+        _ -> %StackUnderflowError{message: "stack underflow occurred, context: " <> context}
+      end
+    end
+  end
+
+  @spec divide([number, ...]) :: integer
+  def divide(list) when length(list) < 2, do: raise StackUnderflowError, "when dividing"
+  def divide([divisor, _dividend]) when 0 == divisor, do: raise DivisionByZeroError
+  def divide([divisor, dividend]), do: div(dividend, divisor)
+
 end
