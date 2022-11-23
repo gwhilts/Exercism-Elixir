133beaea886f08e7a3b2f02a1b5da7cf586c471e
Gerald Hilts <gwhilts@booknowl.com>
2022-11-23 11:45:16 -0800

    Implement 'Anagaram' exercise (accepted)

--- anagram/lib/anagram.ex
+++ anagram/lib/anagram.ex
@@ -4,5 +4,13 @@ defmodule Anagram do
   """
   @spec match(String.t(), [String.t()]) :: [String.t()]
   def match(base, candidates) do
+    Enum.filter(candidates, &(key_for(&1) == key_for(base)))
+    |> Enum.reject(&(String.upcase(&1) == String.upcase(base)))
   end
+
+  # "Tar" -> 'ART', "rat" -> 'ART', "aRt" -> 'ART'
+  defp key_for(word) do
+    String.upcase(word) |> String.to_charlist() |> Enum.sort()
+  end
+
 end
