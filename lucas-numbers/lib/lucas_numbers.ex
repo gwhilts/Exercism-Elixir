f81e8e4610b79f8dae043eb271a07ba94ab471ec
Gerald Hilts <gwhilts@booknowl.com>
2022-11-23 23:36:28 -0800

    Implement 'Lucas Numbers' exercise (accepted)
    Exercism accepted my solution, and I think it's
    clear and effecient, but I suspect that it wasn't
    really what they intended for this exercise.
    
    The overview discussed Streams, but I didn't use them.
    My only Enum calls were to count/1 and reverse/1, so
    there really wasn't anywhere to optimize with a
    Stream.

--- lucas-numbers/lib/lucas_numbers.ex
+++ lucas-numbers/lib/lucas_numbers.ex
@@ -1,11 +1,32 @@
 defmodule LucasNumbers do
   @moduledoc """
-  Lucas numbers are an infinite sequence of numbers which build progressively
+  Lucas numbers are an infinite seriesuence of numbers which build progressively
   which hold a strong correlation to the golden ratio (φ or ϕ)
 
   E.g.: 2, 1, 3, 4, 7, 11, 18, 29, ...
   """
-  def generate(count) do
-    # Please implement the generate/1 function
+
+  def generate(series \\ [], count) do
+    unless is_integer(count), do: raise ArgumentError, "count must be specified as an integer >= 1"
+    unless count >= 1, do: raise ArgumentError, "count must be specified as an integer >= 1"
+    if Enum.count(series) >= count do
+      Enum.reverse series
+    else
+      generate([next_in(series) | series], count)
+    end
   end
+
+  defp next_in(series) do
+    case Enum.count(series) do
+      0 -> 2
+      1 -> 1
+      _ -> sum_of_first_two(series)
+    end
+  end
+
+  defp sum_of_first_two(series) do
+    [a, b | _] = series
+    a + b
+  end
+
 end
