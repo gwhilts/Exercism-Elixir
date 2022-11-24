7410ca339288919328a0cc7f5be06bec5e063be0
Gerald Hilts <gwhilts@booknowl.com>
2022-11-23 16:51:15 -0800

    Refactor 'File Sniffer' exercise (accepted)

--- file-sniffer/lib/file_sniffer.ex
+++ file-sniffer/lib/file_sniffer.ex
@@ -1,39 +1,32 @@
 defmodule FileSniffer do
+  alias ElixirSense.Providers.Suggestion.Reducers.Bitstring
   @media_types [
-    %{ext: "bmp", media_type: "image/bmp", signature: <<0x42, 0x4D>>},
-    %{ext: "exe", media_type: "application/octet-stream", signature: <<0x7F, 0x45, 0x4C, 0x46>>},
-    %{ext: "gif", media_type: "image/gif", signature: <<0x47, 0x49, 0x46>>},
-    %{ext: "jpg", media_type: "image/jpg", signature: <<0xFF, 0xD8, 0xFF>>},
-    %{ext: "png", media_type: "image/png", signature: <<0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A>>}
+    %{ext: "bmp", type: "image/bmp", sig: <<0x42, 0x4D>>},
+    %{ext: "exe", type: "application/octet-stream", sig: <<0x7F, 0x45, 0x4C, 0x46>>},
+    %{ext: "gif", type: "image/gif", sig: <<0x47, 0x49, 0x46>>},
+    %{ext: "jpg", type: "image/jpg", sig: <<0xFF, 0xD8, 0xFF>>},
+    %{ext: "png", type: "image/png", sig: <<0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A>>}
   ]
+
+  @spec type_from_extension(String) :: String
   def type_from_extension(extension) do
-    pluck(:ext, extension) |> Map.get(:media_type)
+    # find a media type with given extention |> return its type
+    Enum.find(@media_types, %{}, &(&1[:ext] == extension)) |> Map.get(:type)
   end
 
+  @spec type_from_binary(Bitstring) :: String
   def type_from_binary(file_binary) do
-    sig = sig_scan(file_binary)
-    pluck(:signature, sig) |> Map.get(:media_type)
+    # find a media type with whose sig the given file_binary begins |> return its type
+    Enum.find(@media_types, %{}, &(String.starts_with? file_binary, &1[:sig])) |> Map.get(:type)
   end
 
+  @spec verify(Bitstring, String) :: {:ok, String} | {:error, String}
   def verify(file_binary, extension) do
-    if type_from_binary(file_binary) == type_from_extension(extension) do
-      {:ok, type_from_extension(extension)}
-    else
-      {:error, "Warning, file format and file extension do not match."}
+    # when the file bin type and extention type are the same, return {:ok, type}, otherwise {:error, ...}
+    case {type_from_binary(file_binary), type_from_extension(extension)} do
+      {type, type} -> {:ok, type}
+      _ -> {:error, "Warning, file format and file extension do not match."}
     end
   end
 
-  @spec pluck(atom(), any) :: Map
-  def pluck(key, val), do: Enum.find(@media_types, %{}, fn(map) -> map[key] == val end)
-
-  def sig_scan(bin) do
-    cond do
-      String.contains?(bin, <<0x42, 0x4D>>) -> <<0x42, 0x4D>>
-      String.contains?(bin, <<0x7F, 0x45, 0x4C, 0x46>>) -> <<0x7F, 0x45, 0x4C, 0x46>>
-      String.contains?(bin, <<0x47, 0x49, 0x46>>) -> <<0x47, 0x49, 0x46>>
-      String.contains?(bin, <<0xFF, 0xD8, 0xFF>>) -> <<0xFF, 0xD8, 0xFF>>
-      String.contains?(bin, <<0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A>>) -> <<0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A>>
-      true -> nil
-    end
-  end
 end
