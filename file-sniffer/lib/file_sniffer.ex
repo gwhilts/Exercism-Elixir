7a107d4f9cd987178d1122ad7b6489c955d58d31
Gerald Hilts <gwhilts@booknowl.com>
2022-11-23 16:04:38 -0800

    Implement 'File Sniffer' exercise (unsub)
    All test passing, but going to refactor
    before submitting.

--- file-sniffer/lib/file_sniffer.ex
+++ file-sniffer/lib/file_sniffer.ex
@@ -1,13 +1,39 @@
 defmodule FileSniffer do
+  @media_types [
+    %{ext: "bmp", media_type: "image/bmp", signature: <<0x42, 0x4D>>},
+    %{ext: "exe", media_type: "application/octet-stream", signature: <<0x7F, 0x45, 0x4C, 0x46>>},
+    %{ext: "gif", media_type: "image/gif", signature: <<0x47, 0x49, 0x46>>},
+    %{ext: "jpg", media_type: "image/jpg", signature: <<0xFF, 0xD8, 0xFF>>},
+    %{ext: "png", media_type: "image/png", signature: <<0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A>>}
+  ]
   def type_from_extension(extension) do
-    # Please implement the type_from_extension/1 function
+    pluck(:ext, extension) |> Map.get(:media_type)
   end
 
   def type_from_binary(file_binary) do
-    # Please implement the type_from_binary/1 function
+    sig = sig_scan(file_binary)
+    pluck(:signature, sig) |> Map.get(:media_type)
   end
 
   def verify(file_binary, extension) do
-    # Please implement the verify/2 function
+    if type_from_binary(file_binary) == type_from_extension(extension) do
+      {:ok, type_from_extension(extension)}
+    else
+      {:error, "Warning, file format and file extension do not match."}
+    end
+  end
+
+  @spec pluck(atom(), any) :: Map
+  def pluck(key, val), do: Enum.find(@media_types, %{}, fn(map) -> map[key] == val end)
+
+  def sig_scan(bin) do
+    cond do
+      String.contains?(bin, <<0x42, 0x4D>>) -> <<0x42, 0x4D>>
+      String.contains?(bin, <<0x7F, 0x45, 0x4C, 0x46>>) -> <<0x7F, 0x45, 0x4C, 0x46>>
+      String.contains?(bin, <<0x47, 0x49, 0x46>>) -> <<0x47, 0x49, 0x46>>
+      String.contains?(bin, <<0xFF, 0xD8, 0xFF>>) -> <<0xFF, 0xD8, 0xFF>>
+      String.contains?(bin, <<0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A>>) -> <<0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A>>
+      true -> nil
+    end
   end
 end
