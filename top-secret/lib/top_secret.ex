40b008bfbb93b87c23857b37a58f43716575246a
Gerald Hilts <gwhilts@booknowl.com>
2022-11-25 15:38:46 -0800

    WIP: tasks 1-4 passing

--- top-secret/lib/top_secret.ex
+++ top-secret/lib/top_secret.ex
@@ -3,8 +3,22 @@ defmodule TopSecret do
     Code.string_to_quoted!(string)
   end
 
-  def decode_secret_message_part(ast, acc) do
-    # Please implement the decode_secret_message_part/2 function
+  def decode_secret_message_part(ast, msg)  do
+    {ast, update_msg(ast, msg)}
+  end
+
+  defp update_msg({:defp, _, args}, msg), do: [code_from(args) | msg]
+  defp update_msg({:def,  _, args}, msg), do: [code_from(args) | msg]
+  defp update_msg(_ast, msg),             do: msg
+
+  defp code_from(args) do
+    [{name, _, arglist},_] = args
+    count = if arglist, do: length(arglist), else: 0
+    cond do
+      name == :when -> code_from(arglist)
+      count == 0 -> ""
+      true -> Atom.to_string(name) |> String.slice(0..(count - 1))
+    end
   end
 
   def decode_secret_message(string) do
