ecd7d4d2d7a5a78abcd5166fb2b42e5f217941dd
Gerald Hilts <gwhilts@booknowl.com>
2022-11-27 10:26:01 -0800

    Implement 'Hamming' exercise (accepted)

--- hamming/lib/hamming.ex
+++ hamming/lib/hamming.ex
@@ -7,7 +7,11 @@ defmodule Hamming do
   iex> Hamming.hamming_distance('AAGTCATA', 'TAGCGATC')
   {:ok, 4}
   """
+
   @spec hamming_distance([char], [char]) :: {:ok, non_neg_integer} | {:error, String.t()}
+  def hamming_distance(strand1, strand2) when length(strand1) != length(strand2), do: {:error, "strands must be of equal length"}
   def hamming_distance(strand1, strand2) do
+    dist = Enum.zip(strand1, strand2) |> Enum.count(fn {s1, s2} -> s1 != s2 end)
+    {:ok, dist}
   end
 end
