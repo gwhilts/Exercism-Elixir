11ccc571ce5376a0ae5adb8ca2412d12f09eb9de
Gerald Hilts <gwhilts@booknowl.com>
2022-11-28 09:33:53 -0800

    Implement 'Rotational Cipher' (accepted)

--- rotational-cipher/lib/rotational_cipher.ex
+++ rotational-cipher/lib/rotational_cipher.ex
@@ -8,5 +8,13 @@ defmodule RotationalCipher do
   """
   @spec rotate(text :: String.t(), shift :: integer) :: String.t()
   def rotate(text, shift) do
+    text
+    |> String.to_charlist()
+    |> Enum.map(fn(char) -> shift_char(char, shift) end)
+    |> List.to_string()
   end
+
+  defp shift_char(char, rot) when char in ?a..?z, do: if char + rot > ?z, do: char + rot - 26, else: char + rot
+  defp shift_char(char, rot) when char in ?A..?Z, do: if char + rot > ?Z, do: char + rot - 26, else: char + rot
+  defp shift_char(char, _), do: char
 end
