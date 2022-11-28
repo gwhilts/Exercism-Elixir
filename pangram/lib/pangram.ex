1b5edfd3d7e618a642bb57324beffbea1bf81ac9
Gerald Hilts <gwhilts@booknowl.com>
2022-11-27 17:08:57 -0800

    Implement 'Pangram' exercise (accepted)

--- pangram/lib/pangram.ex
+++ pangram/lib/pangram.ex
@@ -1,4 +1,5 @@
 defmodule Pangram do
+  @alphabet 'abcdefghijklmnopqrstuvwxyz'
   @doc """
   Determines if a word or sentence is a pangram.
   A pangram is a sentence using every letter of the alphabet at least once.
@@ -14,5 +15,13 @@ defmodule Pangram do
 
   @spec pangram?(String.t()) :: boolean
   def pangram?(sentence) do
+    chars = sentence
+    |> String.downcase()
+    |> String.to_charlist()
+    |> Enum.filter(&( &1 >= ?a and &1 <= ?z))
+    |> Enum.uniq()
+    |> Enum.sort()
+    chars == @alphabet
+
   end
 end
