1c8f4e64504a478b4c0c890a62c21e3428f2afe1
Gerald Hilts <gwhilts@booknowl.com>
2022-11-28 23:28:29 -0800

    Implement 'Run-Length Encoding' (accepted)
    Lessons learned:
    
    - Use String.codepoints/1 to break a string into a list
     of individual character strings (not a charlist).
     `String.codepoints("ABC")`
      => ["A", "B", "C"]
    
    - Use Regex.scan/2 to get a list of *all* matches

--- run-length-encoding/lib/run_length_encoder.ex
+++ run-length-encoding/lib/run_length_encoder.ex
@@ -8,9 +8,28 @@ defmodule RunLengthEncoder do
   """
   @spec encode(String.t()) :: String.t()
   def encode(string) do
+    string
+    |> String.codepoints
+    |> Enum.chunk_by( &(&1) )
+    |> Enum.map( &compact_codepoints/1 )
+    |> Enum.join
   end
 
   @spec decode(String.t()) :: String.t()
-  def decode(string) do
+  def decode(string), do: Regex.scan(~r{(\d*)(.)}, string) |> Enum.reduce("", &expand_codepoints/2 )
+
+
+  defp compact_codepoints(list) do
+    case {length(list), hd(list)} do
+      {1, cp} -> cp
+      {n, cp} -> Integer.to_string(n) <> cp
+    end
+  end
+
+  defp expand_codepoints([_match, count, cp], out) do
+    case count do
+      "" -> out <> cp
+      n -> out <> String.duplicate(cp, String.to_integer(n))
+    end
   end
 end
