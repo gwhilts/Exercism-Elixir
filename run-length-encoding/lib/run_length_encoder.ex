aadbd07d517f7d9dcdf82e3c233d0341d095dcbe
Gerald Hilts <gwhilts@booknowl.com>
2022-11-29 00:17:51 -0800

    Tidy 'Run-Length Encoding'

--- run-length-encoding/lib/run_length_encoder.ex
+++ run-length-encoding/lib/run_length_encoder.ex
@@ -9,15 +9,14 @@ defmodule RunLengthEncoder do
   @spec encode(String.t()) :: String.t()
   def encode(string) do
     string
-    |> String.codepoints
-    |> Enum.chunk_by( &(&1) )
-    |> Enum.map( &compact_codepoints/1 )
-    |> Enum.join
+    |> String.codepoints()
+    |> Enum.chunk_by(& &1)
+    |> Enum.map(&compact_codepoints/1)
+    |> Enum.join()
   end
 
   @spec decode(String.t()) :: String.t()
-  def decode(string), do: Regex.scan(~r{(\d*)(.)}, string) |> Enum.reduce("", &expand_codepoints/2 )
-
+  def decode(string), do: Regex.scan(~r{(\d*)(.)}, string) |> Enum.reduce("", &expand_codepoints/2)
 
   defp compact_codepoints(list) do
     case {length(list), hd(list)} do
