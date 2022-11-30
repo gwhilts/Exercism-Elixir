e6175ae1c2d7588528cc9a8f94bb629f16e05681
Gerald Hilts <gwhilts@booknowl.com>
2022-11-30 12:38:44 -0800

    Implement 'ISBN Verifier' (accepted)

--- isbn-verifier/lib/isbn_verifier.ex
+++ isbn-verifier/lib/isbn_verifier.ex
@@ -1,17 +1,26 @@
 defmodule IsbnVerifier do
-  @doc """
-    Checks if a string is a valid ISBN-10 identifier
-
-    ## Examples
-
-      iex> IsbnVerifier.isbn?("3-598-21507-X")
-      true
-
-      iex> IsbnVerifier.isbn?("3-598-2K507-0")
-      false
-
-  """
   @spec isbn?(String.t()) :: boolean
   def isbn?(isbn) do
+    digits = parse_digits(isbn) |> x_to_10(isbn)
+    valid?(isbn) and rem(checksum(digits), 11) == 0
+  end
+
+  defp checksum(digits) do
+    {checksum, _} = Enum.reduce(digits, {0, 1}, fn(d, {sum, index}) -> {sum + String.to_integer(d) * index, index + 1} end)
+    checksum
+  end
+
+  defp parse_digits(isbn) do
+    Regex.scan(~r/(\d)/, isbn)
+    |> Enum.reduce([], fn([_, match], digits) -> [match | digits] end)
+  end
+
+  defp valid?(isbn), do: String.replace(isbn, "-", "") |> String.match?(~r/^\d{9}[\d|X]?$/)
+
+  defp x_to_10(digits, isbn) do
+    case String.last(isbn) do
+      "X" -> ["10" | digits]
+      _ -> digits
+    end
   end
 end
