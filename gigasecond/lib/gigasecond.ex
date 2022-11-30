4c5aaa0ebf34acc3c2f33981cb4e7ada8c74479a
Gerald Hilts <gwhilts@booknowl.com>
2022-11-30 09:47:48 -0800

    Implement 'Gigasecond' exercise (accepted)

--- gigasecond/lib/gigasecond.ex
+++ gigasecond/lib/gigasecond.ex
@@ -2,8 +2,11 @@ defmodule Gigasecond do
   @doc """
   Calculate a date one billion seconds after an input date.
   """
-  @spec from({{pos_integer, pos_integer, pos_integer}, {pos_integer, pos_integer, pos_integer}}) ::
-          {{pos_integer, pos_integer, pos_integer}, {pos_integer, pos_integer, pos_integer}}
+  @gigasecond 1_000_000_000
+
+  @spec from({{pos_integer, pos_integer, pos_integer}, {pos_integer, pos_integer, pos_integer}}) ::  {{pos_integer, pos_integer, pos_integer}, {pos_integer, pos_integer, pos_integer}}
   def from({{year, month, day}, {hours, minutes, seconds}}) do
+    dt = NaiveDateTime.new!(year, month, day, hours, minutes, seconds) |> NaiveDateTime.add(@gigasecond)
+    {{dt.year, dt.month, dt.day}, {dt.hour, dt.minute, dt.second}}
   end
 end
--- gigasecond/test/gigasecond_test.exs
+++ gigasecond/test/gigasecond_test.exs
@@ -6,22 +6,18 @@ defmodule GigasecondTest do
     assert Gigasecond.from({{2011, 4, 25}, {0, 0, 0}}) == {{2043, 1, 1}, {1, 46, 40}}
   end
 
-  @tag :pending
   test "from 1977-06-13 00:00:00" do
     assert Gigasecond.from({{1977, 6, 13}, {0, 0, 0}}) == {{2009, 2, 19}, {1, 46, 40}}
   end
 
-  @tag :pending
   test "from 1959-19-07 00:00:00" do
     assert Gigasecond.from({{1959, 7, 19}, {0, 0, 0}}) == {{1991, 3, 27}, {1, 46, 40}}
   end
 
-  @tag :pending
   test "from 2015-01-24 22:00:00" do
     assert Gigasecond.from({{2015, 1, 24}, {22, 0, 0}}) == {{2046, 10, 2}, {23, 46, 40}}
   end
 
-  @tag :pending
   test "from 2015-01-24 23:59:59" do
     assert Gigasecond.from({{2015, 1, 24}, {23, 59, 59}}) == {{2046, 10, 3}, {1, 46, 39}}
   end
