74d26dae527b6aab9abb16191401c49819b03b4a
Gerald Hilts <gwhilts@booknowl.com>
2022-11-30 16:36:51 -0800

    Implement 'Nth Prime' exercise (accepted)

--- nth-prime/lib/prime.ex
+++ nth-prime/lib/prime.ex
@@ -1,8 +1,11 @@
 defmodule Prime do
-  @doc """
-  Generates the nth prime.
-  """
+  # Guessing there's a faster way to do this, but brain is failing me at the moment.
+
   @spec nth(non_neg_integer) :: non_neg_integer
-  def nth(count) do
-  end
+  def nth(count) when count > 0, do: nth(2, 0, count)
+  defp nth(num, stop, stop), do: num - 1
+  defp nth(num, count, stop), do: if prime?(num), do: nth(num + 1, count+1, stop), else: nth(num + 1, count, stop)
+
+  defp prime?(num) when num in [1, 2, 3], do: true
+  defp prime?(num), do: Enum.reduce(2..(div(num, 2)), true, &(rem(num, &1) != 0 and &2 ))
 end
