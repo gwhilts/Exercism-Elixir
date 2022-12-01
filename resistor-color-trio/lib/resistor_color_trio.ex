580883110ff6d7c6f7f1c64ac3a7f56c1e9f5f91
Gerald Hilts <gwhilts@booknowl.com>
2022-11-30 19:56:20 -0800

    Refactor 'Resistor Color Trio' (accepted)

--- resistor-color-trio/lib/resistor_color_trio.ex
+++ resistor-color-trio/lib/resistor_color_trio.ex
@@ -1,15 +1,10 @@
 defmodule ResistorColorTrio do
   @colors [:black, :brown, :red, :orange, :yellow, :green, :blue, :violet, :grey, :white]
-  @doc """
-  Calculate the resistance value in ohm or kiloohm from resistor colors
-  """
+
   @spec label(colors :: [atom]) :: {number, :ohms | :kiloohms}
-  def label(colors) do
-    ohms = value(colors)
-    if ohms < 1000, do: {ohms, :ohms}, else: {div(ohms, 1000), :kiloohms}
-  end
+  def label(colors), do: value(colors) |> to_tuple()
 
   defp code(color), do: Enum.find_index(@colors, &(color == &1))
   defp value([c1, c2, c3 | _tail]), do: (code(c1) * 10 + code(c2)) * 10 ** code(c3)
-
+  defp to_tuple(ohms), do: if ohms < 1000, do: {ohms, :ohms}, else: {div(ohms, 1000), :kiloohms}
 end
