826fbddefeb2f7dabb884db8aa96bb4a88e1d53a
Gerald Hilts <gwhilts@booknowl.com>
2022-12-02 16:11:11 -0800

    Implement 'Grade School' exercise (accepted)

--- grade-school/lib/school.ex
+++ grade-school/lib/school.ex
@@ -1,24 +1,22 @@
 defmodule School do
-  @moduledoc """
-  Simulate students in a school.
-
-  Each student is in a grade.
-  """
-
-  @type school :: any()
+  @type school :: [] | [{pos_integer(), String.t()}]
 
   @doc """
   Create a new, empty school.
   """
   @spec new() :: school
-  def new() do
-  end
+  def new(), do: []
 
   @doc """
   Add a student to a particular grade in school.
   """
   @spec add(school, String.t(), integer) :: {:ok | :error, school}
   def add(school, name, grade) do
+    unless name in roster(school) do
+      {:ok, [{grade, name} | school]}
+    else
+      {:error, school}
+    end
   end
 
   @doc """
@@ -26,12 +24,13 @@ defmodule School do
   """
   @spec grade(school, integer) :: [String.t()]
   def grade(school, grade) do
+    Enum.reduce(school,[], fn {g, n}, students -> if g == grade, do: [n | students], else: students end)
+    |> Enum.sort()
   end
 
   @doc """
   Return the names of all the students in the school sorted by grade and name.
   """
   @spec roster(school) :: [String.t()]
-  def roster(school) do
-  end
+  def roster(school), do: Enum.sort(school) |> Enum.map(fn({_grade, name}) -> name end)
 end
--- grade-school/test/school_test.exs
+++ grade-school/test/school_test.exs
@@ -11,123 +11,119 @@ defmodule SchoolTest do
     {Enum.reverse(results), school}
   end
 
-  # @tag :pending
-  test "Roster is empty when no student is added" do
-    assert School.roster(School.new()) == []
+  describe "new/1" do
+    test "returns an empty list" do
+      assert School.new() == []
+    end
   end
 
-  @tag :pending
-  test "Add a student" do
-    {result, school} = School.add(School.new(), "Aimee", 2)
+  describe "roster/1 : " do
+    test "Roster is empty when no student is added" do
+      assert School.roster(School.new()) == []
+    end
 
-    assert result == :ok
-    assert School.roster(school) == ["Aimee"]
+    test "Add a student" do
+      {result, school} = School.add(School.new(), "Aimee", 2)
+
+      assert result == :ok
+      assert School.roster(school) == ["Aimee"]
+    end
+
+    test "Adding multiple students in the same grade in the roster" do
+      students = [{"Blair", 2}, {"James", 2}, {"Paul", 2}]
+      {results, school} = make_school_with_students(students)
+
+      assert results == [:ok, :ok, :ok]
+      assert School.roster(school) == ["Blair", "James", "Paul"]
+    end
+
+    test "Cannot add student to same grade in the roster more than once" do
+      students = [{"Blair", 2}, {"James", 2}, {"James", 2}, {"Paul", 2}]
+      {results, school} = make_school_with_students(students)
+
+      assert results == [:ok, :ok, :error, :ok]
+      assert School.roster(school) == ["Blair", "James", "Paul"]
+    end
+
+    test "Adding students in multiple grades" do
+      students = [{"Chelsea", 3}, {"Logan", 7}]
+      {results, school} = make_school_with_students(students)
+
+      assert results == [:ok, :ok]
+      assert School.roster(school) == ["Chelsea", "Logan"]
+    end
+
+    test "Cannot add same student to multiple grades in the roster" do
+      students = [{"Blair", 2}, {"James", 2}, {"James", 3}, {"Paul", 3}]
+      {results, school} = make_school_with_students(students)
+
+      assert results == [:ok, :ok, :error, :ok]
+      assert School.roster(school) == ["Blair", "James", "Paul"]
+    end
+
+    test "Students are sorted by grades in the roster" do
+      students = [{"Jim", 3}, {"Peter", 2}, {"Anna", 1}]
+      {_results, school} = make_school_with_students(students)
+
+      assert School.roster(school) == ["Anna", "Peter", "Jim"]
+    end
+
+    test "Students are sorted by name in the roster" do
+      students = [{"Peter", 2}, {"Zoe", 2}, {"Alex", 2}]
+      {_results, school} = make_school_with_students(students)
+
+      assert School.roster(school) == ["Alex", "Peter", "Zoe"]
+    end
+
+    test "Students are sorted by grades and then by name in the roster" do
+      students = [
+        {"Peter", 2},
+        {"Anna", 1},
+        {"Barb", 1},
+        {"Zoe", 2},
+        {"Alex", 2},
+        {"Jim", 3},
+        {"Charlie", 1}
+      ]
+
+      {_results, school} = make_school_with_students(students)
+
+      assert School.roster(school) == ["Anna", "Barb", "Charlie", "Alex", "Peter", "Zoe", "Jim"]
+    end
   end
 
-  @tag :pending
-  test "Adding multiple students in the same grade in the roster" do
-    students = [{"Blair", 2}, {"James", 2}, {"Paul", 2}]
-    {results, school} = make_school_with_students(students)
+  describe "grade/2 : " do
+    test "Grade is empty if no students in the roster" do
+      assert School.grade(School.new(), 1) == []
+    end
 
-    assert results == [:ok, :ok, :ok]
-    assert School.roster(school) == ["Blair", "James", "Paul"]
-  end
+    test "Grade is empty if no students in that grade" do
+      students = [{"Peter", 2}, {"Zoe", 2}, {"Alex", 2}, {"Jim", 3}]
+      {_results, school} = make_school_with_students(students)
 
-  @tag :pending
-  test "Cannot add student to same grade in the roster more than once" do
-    students = [{"Blair", 2}, {"James", 2}, {"James", 2}, {"Paul", 2}]
-    {results, school} = make_school_with_students(students)
+      assert School.grade(school, 1) == []
+    end
 
-    assert results == [:ok, :ok, :error, :ok]
-    assert School.roster(school) == ["Blair", "James", "Paul"]
-  end
+    test "Student not added to same grade more than once" do
+      students = [{"Blair", 2}, {"James", 2}, {"James", 2}, {"Paul", 2}]
+      {_results, school} = make_school_with_students(students)
 
-  @tag :pending
-  test "Adding students in multiple grades" do
-    students = [{"Chelsea", 3}, {"Logan", 7}]
-    {results, school} = make_school_with_students(students)
+      assert School.roster(school) == ["Blair", "James", "Paul"]
+    end
 
-    assert results == [:ok, :ok]
-    assert School.roster(school) == ["Chelsea", "Logan"]
-  end
+    test "Student not added to multiple grades" do
+      students = [{"Blair", 2}, {"James", 2}, {"James", 3}, {"Paul", 3}]
+      {_results, school} = make_school_with_students(students)
 
-  @tag :pending
-  test "Cannot add same student to multiple grades in the roster" do
-    students = [{"Blair", 2}, {"James", 2}, {"James", 3}, {"Paul", 3}]
-    {results, school} = make_school_with_students(students)
+      assert School.grade(school, 2) == ["Blair", "James"]
+      assert School.grade(school, 3) == ["Paul"]
+    end
 
-    assert results == [:ok, :ok, :error, :ok]
-    assert School.roster(school) == ["Blair", "James", "Paul"]
-  end
+    test "Students are sorted by name in a grade" do
+      students = [{"Franklin", 5}, {"Bradley", 5}, {"Jeff", 1}]
+      {_results, school} = make_school_with_students(students)
 
-  @tag :pending
-  test "Students are sorted by grades in the roster" do
-    students = [{"Jim", 3}, {"Peter", 2}, {"Anna", 1}]
-    {_results, school} = make_school_with_students(students)
-
-    assert School.roster(school) == ["Anna", "Peter", "Jim"]
-  end
-
-  @tag :pending
-  test "Students are sorted by name in the roster" do
-    students = [{"Peter", 2}, {"Zoe", 2}, {"Alex", 2}]
-    {_results, school} = make_school_with_students(students)
-
-    assert School.roster(school) == ["Alex", "Peter", "Zoe"]
-  end
-
-  @tag :pending
-  test "Students are sorted by grades and then by name in the roster" do
-    students = [
-      {"Peter", 2},
-      {"Anna", 1},
-      {"Barb", 1},
-      {"Zoe", 2},
-      {"Alex", 2},
-      {"Jim", 3},
-      {"Charlie", 1}
-    ]
-
-    {_results, school} = make_school_with_students(students)
-
-    assert School.roster(school) == ["Anna", "Barb", "Charlie", "Alex", "Peter", "Zoe", "Jim"]
-  end
-
-  @tag :pending
-  test "Grade is empty if no students in the roster" do
-    assert School.grade(School.new(), 1) == []
-  end
-
-  @tag :pending
-  test "Grade is empty if no students in that grade" do
-    students = [{"Peter", 2}, {"Zoe", 2}, {"Alex", 2}, {"Jim", 3}]
-    {_results, school} = make_school_with_students(students)
-
-    assert School.grade(school, 1) == []
-  end
-
-  @tag :pending
-  test "Student not added to same grade more than once" do
-    students = [{"Blair", 2}, {"James", 2}, {"James", 2}, {"Paul", 2}]
-    {_results, school} = make_school_with_students(students)
-
-    assert School.roster(school) == ["Blair", "James", "Paul"]
-  end
-
-  @tag :pending
-  test "Student not added to multiple grades" do
-    students = [{"Blair", 2}, {"James", 2}, {"James", 3}, {"Paul", 3}]
-    {_results, school} = make_school_with_students(students)
-
-    assert School.grade(school, 2) == ["Blair", "James"]
-    assert School.grade(school, 3) == ["Paul"]
-  end
-
-  @tag :pending
-  test "Students are sorted by name in a grade" do
-    students = [{"Franklin", 5}, {"Bradley", 5}, {"Jeff", 1}]
-    {_results, school} = make_school_with_students(students)
-
-    assert School.grade(school, 5) == ["Bradley", "Franklin"]
+      assert School.grade(school, 5) == ["Bradley", "Franklin"]
+    end
   end
 end
