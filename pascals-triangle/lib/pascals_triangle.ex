41bf21cd49182e9b93cbe16b6c3f44960a7c7f43
Gerald Hilts <gwhilts@booknowl.com>
2022-12-04 13:43:30 -0800

    Implement 'Pascals Triangle' (accepted)

--- pascals-triangle/lib/pascals_triangle.ex
+++ pascals-triangle/lib/pascals_triangle.ex
@@ -4,6 +4,6 @@ defmodule PascalsTriangle do
   with the given height
   """
   @spec rows(integer) :: [[integer]]
-  def rows(num) do
-  end
+  def rows(num), do: Stream.iterate([1], &next_row/1) |> Enum.take(num)
+  defp next_row(prev), do: [1 | (for i <- 0..(length(prev) - 1), do: Enum.at(prev, i) + (Enum.at(prev, i + 1) || 0))]
 end
