1d5fe68bf5cdca633963b6d8befdeb3c79ac1435
Gerald Hilts <gwhilts@booknowl.com>
2022-12-04 12:16:50 -0800

    Implement 'Matrix' exercise (accepted)

--- matrix/lib/matrix.ex
+++ matrix/lib/matrix.ex
@@ -1,47 +1,17 @@
 defmodule Matrix do
-  defstruct matrix: nil
+  # Don't know if this was cheating, but I just used a nested
+  # list instead of the %Matrix{} struct suggested. It required
+  # much less foofing around. Also, note that this code assumes
+  # each row has an equal number of columns, but so did the tests
+  # so I didn't worry about working around that.
 
-  @doc """
-  Convert an `input` string, with rows separated by newlines and values
-  separated by single spaces, into a `Matrix` struct.
-  """
-  @spec from_string(input :: String.t()) :: %Matrix{}
-  def from_string(input) do
-  end
+  def columns(matrix), do: for col <- 1..length(Enum.at(matrix, 0)), do: column(matrix, col)
+  def column(matrix, index), do: for row <- matrix, do: Enum.at(row, index - 1)
+  def from_string(input), do: String.split(input, "\n") |> Enum.map(&to_ints/1)
+  def row(matrix, index), do: Enum.at matrix, (index - 1)
+  def rows(matrix), do: matrix
+  def to_string(matrix), do: Enum.map(matrix, & (Enum.join &1, " ")) |> Enum.join("\n")
 
-  @doc """
-  Write the `matrix` out as a string, with rows separated by newlines and
-  values separated by single spaces.
-  """
-  @spec to_string(matrix :: %Matrix{}) :: String.t()
-  def to_string(matrix) do
-  end
-
-  @doc """
-  Given a `matrix`, return its rows as a list of lists of integers.
-  """
-  @spec rows(matrix :: %Matrix{}) :: list(list(integer))
-  def rows(matrix) do
-  end
-
-  @doc """
-  Given a `matrix` and `index`, return the row at `index`.
-  """
-  @spec row(matrix :: %Matrix{}, index :: integer) :: list(integer)
-  def row(matrix, index) do
-  end
-
-  @doc """
-  Given a `matrix`, return its columns as a list of lists of integers.
-  """
-  @spec columns(matrix :: %Matrix{}) :: list(list(integer))
-  def columns(matrix) do
-  end
-
-  @doc """
-  Given a `matrix` and `index`, return the column at `index`.
-  """
-  @spec column(matrix :: %Matrix{}, index :: integer) :: list(integer)
-  def column(matrix, index) do
-  end
+  # private
+  defp to_ints(string), do: String.split(string) |> Enum.map(&String.to_integer/1)
 end
