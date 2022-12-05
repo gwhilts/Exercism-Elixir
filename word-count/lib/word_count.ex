2cc947c1fa32bbcd02f7e1a16a78f5f2d35a3d8f
Gerald Hilts <gwhilts@booknowl.com>
2022-12-04 21:27:10 -0800

    Implement 'Word Count' exercise (accepted)

--- word-count/lib/word_count.ex
+++ word-count/lib/word_count.ex
@@ -5,6 +5,11 @@ defmodule WordCount do
   Words are compared case-insensitively.
   """
   @spec count(String.t()) :: map
-  def count(sentence) do
+  def count(sentence), do: word_list(sentence) |> Enum.frequencies()
+
+  defp word_list(sentence) do
+    String.downcase(sentence)
+    |> String.split(~r/[ \:\._\n,!!&@\$%\^]+/, trim: true)
+    |> Enum.map(&(String.trim(&1, "'")))
   end
 end
