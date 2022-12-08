2172606227be318bbfccd256f39b665221e3b184
Gerald Hilts <gwhilts@booknowl.com>
2022-12-07 19:57:05 -0800

    Implement 'Prime Factors' exercise (accepted)
    All tests are passing, and the exercise was accepted by
    Exercism, but the :slow test doesn't complete within the
    optional timeout.
    
    TODO: Revisit this exercise and find an algorithm that
    passes the timeout flag.

--- prime-factors/lib/prime_factors.ex
+++ prime-factors/lib/prime_factors.ex
@@ -8,6 +8,9 @@ defmodule PrimeFactors do
   The prime factors of 'number' will be ordered lowest to highest.
   """
   @spec factors_for(pos_integer) :: [pos_integer]
-  def factors_for(number) do
-  end
+  def factors_for(num), do: factors_for(num, 2, []) |> Enum.sort
+
+  defp factors_for(1, _, acc), do: acc
+  defp factors_for(num, maybe, acc) when rem(num, maybe) == 0, do: factors_for(div(num, maybe), maybe, [maybe | acc])
+  defp factors_for(num, maybe, acc), do: factors_for(num, maybe + 1, acc)
 end
