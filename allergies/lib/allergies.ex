30691291208b633748f7cc8b4ac649f97cae7cc6
Gerald Hilts <gwhilts@booknowl.com>
2022-12-09 21:11:54 -0800

    Implement 'Allergies' exercise (accepted)
    This exercise seemed was marked "medium", but seemed incredibly
    easy. You simply needed to import the Bitwise module and add
    a couple lines to each of the predefined functions. I suspect that
    the authors might have intended us to write our own implmentation of
    a bitwise-and operation.

--- allergies/lib/allergies.ex
+++ allergies/lib/allergies.ex
@@ -1,15 +1,19 @@
 defmodule Allergies do
+  import Bitwise
+  @allergies %{"eggs" => 1, "peanuts" => 2, "shellfish" => 4, "strawberries" => 8, "tomatoes" => 16, "chocolate" => 32, "pollen" => 64, "cats" => 128}
+
   @doc """
   List the allergies for which the corresponding flag bit is true.
   """
   @spec list(non_neg_integer) :: [String.t()]
   def list(flags) do
+    Enum.reduce(@allergies, [], fn {allergen, val}, lst -> if (flags &&& val) == val, do: [allergen | lst], else: lst end)
+    |> Enum.reverse()
   end
 
   @doc """
   Returns whether the corresponding flag bit in 'flags' is set for the item.
   """
   @spec allergic_to?(non_neg_integer, String.t()) :: boolean
-  def allergic_to?(flags, item) do
-  end
+  def allergic_to?(flags, item), do: (flags &&& Map.get(@allergies, item)) == Map.get(@allergies, item)
 end
