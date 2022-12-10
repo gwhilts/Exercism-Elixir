90b53cdd94c79fac35d49ac9b31524a29a7c8e65
Gerald Hilts <gwhilts@booknowl.com>
2022-12-10 12:57:26 -0800

    WIP: Implement Queens.new/1 (passing)

--- queen-attack/lib/queens.ex
+++ queen-attack/lib/queens.ex
@@ -7,6 +7,28 @@ defmodule Queens do
   """
   @spec new(Keyword.t()) :: Queens.t()
   def new(opts \\ []) do
+    # Enum.reduce(opts, %Queens{}, fn(opt, queens) -> add_queen(opt, queens) end)
+    Enum.reduce(opts, %Queens{}, &add_queen/2)
+  end
+
+  # def add_queen(q, [color => {row, coll}]) when color in [:red, :white] and row in 1..8 and col in 1..8 do
+
+  # end
+
+  @spec add_queen({atom, {pos_integer, pos_integer}}, %Queens{}) :: %Queens{}
+  def add_queen({color, {row, col}}, queens) do
+    if valid_queen?({color, {row, col}}, queens) do
+      %{queens | color => {row, col}}
+    else
+      raise ArgumentError
+    end
+  end
+
+  def valid_queen?({color, {row, col}}, queens) do
+    color in [:white, :black]
+    && row in 0..7
+    && col in 0..7
+    && !({row, col} in Map.values(queens))
   end
 
   @doc """
