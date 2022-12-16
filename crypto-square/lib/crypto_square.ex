e04473528be0007cf6da1f5e07e2fcd1a60dbc5b
Gerald Hilts <gwhilts@booknowl.com>
2022-12-16 12:21:04 -0800

    WIP 'Crypto Square'
    All tests passing with msgs that do
    not require padding spaces.

--- crypto-square/lib/crypto_square.ex
+++ crypto-square/lib/crypto_square.ex
@@ -7,6 +7,36 @@ defmodule CryptoSquare do
     "ac bd"
   """
   @spec encode(String.t()) :: String.t()
-  def encode(str) do
+  def encode(""), do: ""
+  def encode(msg) do
+    msg
+    |> String.downcase()
+    |> String.replace(~r/[^a-z0-9]/, "")
+    |> to_norm_grid()
+    |> to_enc_grid()
+    |> Enum.map(fn(row) -> Enum.join(row) end)
+    |> Enum.join(" ")
+  end
+
+  #spec to_norm_grid(String.t()) :: {[[String.t()]], {pos_integer, pos_integer}}
+  defp to_norm_grid(str) do
+    {_r, c} = demensions_for(str)
+    String.codepoints(str) |> Enum.chunk_every(c)
+  end
+
+  #spec to_enc_grid([[binary]]) :: [[binary]]
+  def to_enc_grid(norm) do
+    last_row = (Enum.count(norm) - 1)
+    last_col = (Enum.count(Enum.at(norm, 0)) - 1)
+    (for c <- 0..last_col, r <- 0..last_row, do: Enum.at(Enum.at(norm, r, [" "]), c, " "))
+    |> Enum.chunk_every(last_col + 1)
+  end
+
+  #spec demensions_for(binary) :: {integer, integer}
+  # demensions_for("abcd") :: {2, 2}
+  defp demensions_for(str) do
+    r = trunc(:math.sqrt(String.length(str)))
+    c = round(String.length(str) / r)
+    {r, c}
   end
 end
--- crypto-square/test/crypto_square_test.exs
+++ crypto-square/test/crypto_square_test.exs
@@ -1,32 +1,26 @@
 defmodule CryptoSquareTest do
   use ExUnit.Case
 
-  # @tag :pending
   test "empty plaintext results in an empty ciphertext" do
     assert CryptoSquare.encode("") == ""
   end
 
-  @tag :pending
   test "lowercase" do
     assert CryptoSquare.encode("A") == "a"
   end
 
-  @tag :pending
   test "remove spaces" do
     assert CryptoSquare.encode("  b ") == "b"
   end
 
-  @tag :pending
   test "remove punctuation" do
     assert CryptoSquare.encode("@1,%!") == "1"
   end
 
-  @tag :pending
   test "9 character plaintext results in 3 chunks of 3 characters" do
     assert CryptoSquare.encode("This is fun!") == "tsf hiu isn"
   end
 
-  @tag :pending
   test "8 character plaintext results in 3 chunks, the last one with a trailing space" do
     assert CryptoSquare.encode("Chill out.") == "clu hlt io "
   end
