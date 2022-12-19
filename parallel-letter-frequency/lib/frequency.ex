2ced4ab82af7bfd9abcbd1231893c9a38fc952d1
Gerald Hilts <gwhilts@booknowl.com>
2022-12-19 11:28:50 -0800

    WIP: 'Parallel Letter Frequency' (passing)
    All tests are passing, but I'm not making use of the workers
    argument to calculate the frequencies in parallel. (Though I do
    wonder if Enum.frequencies/1 might not be using multiple processes
    under the covers.)
    
    TODO: Figure out how to break up the job into parts, farm them out
    to task processes, combine the results.

--- parallel-letter-frequency/lib/frequency.ex
+++ parallel-letter-frequency/lib/frequency.ex
@@ -7,6 +7,12 @@ defmodule Frequency do
   The number of worker processes to use can be set with 'workers'.
   """
   @spec frequency([String.t()], pos_integer) :: map
-  def frequency(texts, workers) do
+  def frequency([], _), do: %{}
+  def frequency(texts, _workers) do
+    Enum.join(texts)
+    |> String.downcase()
+    |> String.replace(~r/[^a-zà-ÿ]/, "")
+    |> String.codepoints()
+    |> Enum.frequencies()
   end
 end
