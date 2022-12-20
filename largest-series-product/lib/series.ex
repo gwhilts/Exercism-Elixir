ca6fc5b8c5bb76a131acc5a2f1f07e0a4630a67c
Gerald Hilts <gwhilts@booknowl.com>
2022-12-20 10:36:54 -0800

    Implement 'Largest Series Product' (accepted)

--- largest-series-product/lib/series.ex
+++ largest-series-product/lib/series.ex
@@ -3,6 +3,32 @@ defmodule Series do
   Finds the largest product of a given number of consecutive numbers in a given string of numbers.
   """
   @spec largest_product(String.t(), non_neg_integer) :: non_neg_integer
+  def largest_product(_, 0), do: 1
   def largest_product(number_string, size) do
+    if valid?(number_string, size) do
+      String.codepoints(number_string)
+      |> Enum.chunk_every(size, 1, :discard)
+      |> Enum.map(&product/1)
+      |> Enum.max
+    else
+      raise ArgumentError
+    end
+  end
+
+  # Private
+
+  defp product(digits) do
+    Enum.map(digits, &String.to_integer/1)
+    |> Enum.reduce(1, &*/2)
+  end
+
+  defp valid?(nstring, size) do
+    cond do
+      nstring == "" -> false
+      String.length(nstring) < size -> false
+      Regex.match? ~r/[^\d]/, nstring -> false
+      size < 0 -> false
+      true -> true
+    end
   end
 end
