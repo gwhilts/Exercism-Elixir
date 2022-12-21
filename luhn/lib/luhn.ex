548b56b0de637a11333bdf821ef6a50a58b55c20
Gerald Hilts <gwhilts@booknowl.com>
2022-12-20 19:43:42 -0800

    Implement 'Luhn' exercise (accepted)

--- luhn/lib/luhn.ex
+++ luhn/lib/luhn.ex
@@ -3,6 +3,22 @@ defmodule Luhn do
   Checks if the given number is valid via the luhn formula
   """
   @spec valid?(String.t()) :: boolean
-  def valid?(number) do
+  def valid?(string) do
+    num = String.replace(string, " ", "")
+    String.length(num) > 1
+    and !(Regex.match?(~r/[^\d]/, num))
+    and rem(checksum(num), 10) == 0
   end
+
+  # Private
+
+  defp checksum(digits) do
+    String.codepoints(digits)
+    |> Enum.reverse()
+    |> Enum.map(&String.to_integer/1)
+    |> Enum.with_index()
+    |> Enum.reduce(0, fn {n, i}, sum -> if rem(i, 2) == 0, do: sum + n, else: sum + dbl_dig(n) end)
+  end
+
+  defp dbl_dig(n), do: if n * 2 > 9, do: (n * 2) - 9, else: (n * 2)
 end
