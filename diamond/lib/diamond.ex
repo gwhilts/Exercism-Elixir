39e258985ebaa0ab9e7912cefcdeda8c7f919d3f
Gerald Hilts <gwhilts@booknowl.com>
2023-01-03 12:23:49 -0800

    Implement 'Diamond' exercise (accepted)

--- diamond/lib/diamond.ex
+++ diamond/lib/diamond.ex
@@ -3,7 +3,24 @@ defmodule Diamond do
   Given a letter, it prints a diamond starting with 'A',
   with the supplied letter at the widest point.
   """
-  @spec build_shape(char) :: String.t()
-  def build_shape(letter) do
+  # @spec build_shape(char) :: String.t()
+  def build_shape(char) do
+    center = row(char, 0)
+    width = String.length(center)
+    top = rows_to(char, width)
+    Enum.join(top) <> center <> "\n" <>Enum.join(Enum.reverse(top))
   end
+
+  defp row(?A, 0), do: "A"
+  defp row(char, 0), do: <<char>> <> String.duplicate(" ", spaces(char)) <> <<char>>
+  defp row(char, width) do
+    core = row(char, 0)
+    pad = String.duplicate(" ", div((width - String.length(core)), 2))
+    pad <> core <> pad <> "\n"
+  end
+
+  defp rows_to(?A, _), do: [""]
+  defp rows_to(char, width), do: for i <- ?A..(char - 1), do: row(i, width)
+
+  defp spaces(char), do: (char - 64) * 2 - 3
 end
