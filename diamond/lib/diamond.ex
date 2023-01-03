7f8af0304225c0bdd2a7079eec32a28ffe924a10
Gerald Hilts <gwhilts@booknowl.com>
2023-01-03 12:51:40 -0800

    Tidy 'Diamond'

--- diamond/lib/diamond.ex
+++ diamond/lib/diamond.ex
@@ -5,22 +5,20 @@ defmodule Diamond do
   """
   # @spec build_shape(char) :: String.t()
   def build_shape(char) do
-    center = row(char, 0)
-    width = String.length(center)
+    width = (char - 64) * 2 - 1
     top = rows_to(char, width)
-    Enum.join(top) <> center <> "\n" <> Enum.join(Enum.reverse(top))
+    Enum.join(top) <> Enum.join(tl(Enum.reverse(top)))
   end
 
-  defp row(?A, 0), do: "A"
-  defp row(char, 0), do: <<char>> <> String.duplicate(" ", spaces(char)) <> <<char>>
+  defp row(?A), do: "A"
+  defp row(char), do: <<char>> <> String.duplicate(" ", spaces(char)) <> <<char>>
   defp row(char, width) do
-    core = row(char, 0)
+    core = row(char)
     pad = String.duplicate(" ", div((width - String.length(core)), 2))
     pad <> core <> pad <> "\n"
   end
 
-  defp rows_to(?A, _), do: [""]
-  defp rows_to(char, width), do: for c <- ?A..(char - 1), do: row(c, width)
+  defp rows_to(char, width), do: for c <- ?A..(char), do: row(c, width)
 
   defp spaces(char), do: (char - 64) * 2 - 3
 end
