c9ad9614289487067ccef4778e30e4f66b28aa5b
Gerald Hilts <gwhilts@booknowl.com>
2023-01-08 00:24:30 -0800

    Implement 'Meetup' exercise (accepted)

--- meetup/lib/meetup.ex
+++ meetup/lib/meetup.ex
@@ -2,16 +2,10 @@ defmodule Meetup do
   @moduledoc """
   Calculate meetup dates.
   """
+  @wday %{monday: 1, tuesday: 2, wednesday: 3, thursday: 4, friday: 5, saturday: 6, sunday: 7}
+  @base_day %{first: 1, second: 8, teenth: 13, third: 15, fourth: 22}
 
-  @type weekday ::
-          :monday
-          | :tuesday
-          | :wednesday
-          | :thursday
-          | :friday
-          | :saturday
-          | :sunday
-
+  @type weekday :: :monday | :tuesday | :wednesday | :thursday | :friday | :saturday | :sunday
   @type schedule :: :first | :second | :third | :fourth | :last | :teenth
 
   @doc """
@@ -21,6 +15,19 @@ defmodule Meetup do
   fall.
   """
   @spec meetup(pos_integer, pos_integer, weekday, schedule) :: Date.t()
-  def meetup(year, month, weekday, schedule) do
-  end
+  def meetup(year, month, weekday, :last), do:
+    Date.new!(year, month, 1) |> Date.end_of_month() |> prev_weekday(weekday)
+
+  def meetup(year, month, weekday, schedule), do:
+    Date.new!(year, month, @base_day[schedule]) |> next_weekday(weekday)
+
+  defp ahead(wday1, wday2) when wday1 <= wday2, do: wday2 - wday1
+  defp ahead(wday1, wday2), do: (7 - wday1) + wday2
+
+  defp back(wday1, wday2) when wday1 >= wday2, do: wday2 - wday1
+  defp back(wday1, wday2), do: (wday2 - 7) - wday1
+
+  defp next_weekday(date, weekday), do: Date.add(date, ahead(Date.day_of_week(date), @wday[weekday]))
+
+  defp prev_weekday(date, weekday), do: Date.add(date, back(Date.day_of_week(date), @wday[weekday]))
 end
