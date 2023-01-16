8022b581d98065e504f244e55345f57e65fcdc05
Gerald Hilts <gwhilts@booknowl.com>
2023-01-16 15:40:27 -0800

    Implement 'Sieve' exercise (accepted)

--- sieve/lib/sieve.ex
+++ sieve/lib/sieve.ex
@@ -3,6 +3,11 @@ defmodule Sieve do
   Generates a list of primes up to a given limit.
   """
   @spec primes_to(non_neg_integer) :: [non_neg_integer]
-  def primes_to(limit) do
-  end
+  def primes_to(limit) when limit < 2, do: []
+  def primes_to(limit), do: sift(Enum.to_list(2..limit), [])
+
+  defp sift([p | []], primes), do: Enum.reverse([p | primes])
+  defp sift([p | tail], primes), do: sift(filter_factors(p, tail), [p | primes])
+
+  defp filter_factors(base, list), do: list -- Enum.to_list((base * 2)..List.last(list)//base)
 end
