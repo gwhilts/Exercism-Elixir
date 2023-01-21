1fb0b9e02a708ebd3c0bb2d93619329e89e082a3
Gerald Hilts <gwhilts@booknowl.com>
2023-01-20 23:04:00 -0800

    Squashed commit of the following:
    commit b09ff5543a359ea5262b57a0498ffed72490a171
    Author: Gerald Hilts <gwhilts@booknowl.com>
    Date:   Thu Jan 19 17:09:11 2023 -0800
    
        Sieve walkthrough complete
    
    commit 40d70db61de8f272d2b35d6f31360d88b5c81d80
    Author: Gerald Hilts <gwhilts@booknowl.com>
    Date:   Thu Jan 19 17:05:32 2023 -0800
    
        step_05 add guard to primes_to/1
    
    commit 0c4e390d0df90a457d3c5ab6112912426d3f4284
    Author: Gerald Hilts <gwhilts@booknowl.com>
    Date:   Thu Jan 19 17:02:15 2023 -0800
    
        step_04 reverse return list
    
    commit 0a47dcbcf96f4d1a65a6d7737328146015a7c4a0
    Author: Gerald Hilts <gwhilts@booknowl.com>
    Date:   Thu Jan 19 16:58:44 2023 -0800
    
        step_03 sift/2
    
    commit b253476c46c603bcfa5dd3d55aaaf30c3c93db7e
    Author: Gerald Hilts <gwhilts@booknowl.com>
    Date:   Thu Jan 19 16:50:36 2023 -0800
    
        step_02 implment primes_to/1
    
    commit 6d26551da20e972509a1067cabe0c3639a538728
    Author: Gerald Hilts <gwhilts@booknowl.com>
    Date:   Thu Jan 19 16:49:20 2023 -0800
    
        step_01 implement remove_multiples/1

--- sieve_walkthrough/lib/sieve.ex
+++ sieve_walkthrough/lib/sieve.ex
@@ -3,6 +3,11 @@ defmodule Sieve do
   Generates a list of primes up to a given limit.
   """
   @spec primes_to(non_neg_integer) :: [non_neg_integer]
-  def primes_to(limit) do
-  end
+  def primes_to(limit) when limit < 2, do: []
+  def primes_to(limit), do: sift(Enum.to_list(2..limit), [])
+
+  defp sift([p | []], primes), do: Enum.reverse([p | primes])
+  defp sift([p | tail], primes), do: sift(remove_multiples(p, tail), [p | primes])
+
+  defp remove_multiples(base, list), do: list -- Enum.to_list(base..List.last(list)//base)
 end
